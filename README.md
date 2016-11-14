008bdddadc9bd5b594c5e768736f34dd5ba292bb
Mathias Schilling <m@matchilling.com>
2016-11-14 23:53:56 +0000

    Init project :poop:

---
+++ README.md
@@ -0,0 +1,39 @@
+# My Personal JavaScript Styleguide
+
+My personal JavaScript styleguide derives mainly from the [AirBnb JS rule book](https://github.com/airbnb/javascript) with some little tweeks which are denoted in the repository.
+
+## Usage
+
+Add `eslint-config-matchilling` to your `package.json`.
+
+```json
+{
+    "devDependencies": {
+        "eslint-config-matchilling": "^1.0.0",
+    },
+    "eslintConfig": {
+        "extends": [
+            "eslint-config-matchilling"
+        ]
+    }
+}
+```
+
+## Recommended editor plugins
+
+- [atom](https://atom.io/packages/eslint)
+- [VIM](https://github.com/vim-syntastic/syntastic/tree/master/syntax_checkers/javascript)
+
+For further integrations check [eslint.org integrations website](http://eslint.org/docs/user-guide/integrations).
+
+## License
+
+This distribution is covered by the **GNU GENERAL PUBLIC LICENSE**, Version 3, 29 June 2007.
+
+## Support & Contact
+
+Having trouble with this repository? Check out the documentation at the repository's site or contact m@matchilling.com and we'll help you sort it out.
+
+Happy Coding
+
+:v:
---
+++ src/index.js
@@ -0,0 +1,102 @@
+module.exports = {
+  extends : [
+    'eslint-config-airbnb'
+  ].map(require.resolve),
+  globals : {
+    it       : true,
+    describe : true
+  },
+  rules : {
+    /**
+     * @see http://eslint.org/docs/rules/brace-style
+     */
+    'brace-style' : ['error', '1tbs', {
+      allowSingleLine : true
+    }],
+
+    /**
+     * @see http://eslint.org/docs/rules/brace-style
+     */
+    'comma-dangle' : ['error', 'never'],
+
+    /**
+     * @see http://eslint.org/docs/rules/func-names
+     */
+    'func-names' : ['error', 'never'],
+
+    /**
+     * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+     */
+    'import/no-extraneous-dependencies' : ['error', {
+      devDependencies      : true,
+      optionalDependencies : false
+    }],
+
+    /**
+     * @see http://eslint.org/docs/rules/indent
+     */
+    indent : ['error', 2, {
+      VariableDeclarator : {
+        var   : 2,
+        let   : 2,
+        const : 3
+      }
+    }],
+
+    /**
+     * @see http://eslint.org/docs/rules/key-spacing
+     */
+    'key-spacing' : ['error', {
+      afterColon  : true,
+      beforeColon : true,
+      align       : 'colon'
+    }],
+
+    /**
+     * @see http://eslint.org/docs/rules/no-prototype-builtins
+     */
+    'no-prototype-builtins' : 'off',
+
+    /**
+     * @see http://eslint.org/docs/rules/no-use-before-define
+     */
+    'no-use-before-define' : 'off',
+
+    /**
+     * @see http://eslint.org/docs/rules/object-curly-newline
+     */
+    'object-curly-newline' : ['error', {
+      ObjectExpression : 'always',
+      ObjectPattern    : 'never'
+    }],
+
+    /**
+     * @see http://eslint.org/docs/rules/one-var
+     */
+    'one-var' : ['warn', 'always'],
+
+    /**
+     * @see http://eslint.org/docs/rules/quotes
+     */
+    quotes : ['error', 'single'],
+
+    /**
+     * @see http://eslint.org/docs/rules/quote-props
+     */
+    'quote-props' : ['warn', 'as-needed', {
+      keywords    : false,
+      unnecessary : true
+    }],
+
+    /**
+     * @see http://eslint.org/docs/rules/strict
+     */
+    strict : 'off',
+
+    /**
+     * @see http://eslint.org/docs/rules/yoda
+     */
+    yoda : ['error', 'always']
+  }
+
+};
